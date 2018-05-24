ebb57db40a34533af4c5598057e71333684297cf
Zhiwen Huang <zhiwen555@gmail.com>
2018-05-24 12:54:28 -0700

    Added a way to display all contracts with cli

---
+++ src/cli/contracts.js
@@ -0,0 +1,29 @@
+'use strict';
+const path = require('path');
+const fs = require('fs');
+const optionActions = require('./optionActions');
+const eth = require('./../ethereum/ethereum');
+const config = require('./../config/config');
+
+/**
+ * Displays built contracts
+ */
+module.exports = (options) => {
+  optionActions(options, 'none');
+  const builtPath = path.resolve(config.paths.built);
+  let contracts = fs.readdirSync(builtPath);
+  contracts = contracts.filter(contract => {
+    return contract.endsWith('.sol.js');
+  }).map(contract => {
+    return path.parse(contract).name;
+  }).map(contract => {
+    return path.parse(contract).name;
+  });
+
+  if (contracts.length === 0) {
+    console.log('No built contracts');
+  } else {
+    console.log('Built contracts:', contracts.join(','));
+  }
+
+};
